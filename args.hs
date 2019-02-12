ae7408cc8bb1970de491f8887eeb0f007ee79b00
ccQ <ccqpein@protonmail.com>
2019-02-12 15:49:57 -0500

    start to give jsonx argv

--- args.hs
+++ args.hs
@@ -5,7 +5,8 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 data Args = Args {
   filetypes  :: [String],
     dir      :: String,
-    keywords :: [String]
+    keywords :: [String],
+    jsonx    :: String
   } deriving (Show,Eq)
 
 parse_args :: [String] -> Args -> Args
@@ -14,9 +15,8 @@ parse_args (x:y:xs) a
   | x == "-f" || x == "-filetype" = parse_args xs (a {filetypes = (map ("." ++) (handle_mutil_str y)) ++ (filetypes a) })
   | x == "-k" || x == "-keyword" = parse_args xs (a {keywords = (handle_mutil_str y) ++ (keywords a) })
   | x == "-d" || x == "-dir" = parse_args xs (a {dir = y })
+  | x == "j" || x == "-jsonx" = parse_args xs (a {jsonx = y})
   |otherwise = a
-
-
 
 handle_mutil_str :: [Char] -> [[Char]]
 handle_mutil_str = (filter (not.null))
@@ -27,4 +27,4 @@ handle_mutil_str = (filter (not.null))
   .(BL.pack)
 
 init_args :: Args
-init_args = Args [] "." []
+init_args = Args [] "." [] ""
